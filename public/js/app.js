6c2d9952bd5db83c94bd066cb2ffe6438af183ce
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-01-06 18:14:46 -0600

    Agregamos la funcionalidad de agregar las citas en un listado, ademas de habilitar un boton para eliminar las citas que sean canceladas o eliminadas

--- public/js/app.js
+++ public/js/app.js
@@ -16,9 +16,12 @@ class Appointments {
         this.appointments = [];
     }
 
-    addApoinment(appoinment) {
-        this.appointments = [ ...this.appointments, appoinment];
-        console.log(this.appointments);
+    addAppointment(appointment) {
+        this.appointments = [ ...this.appointments, appointment];
+    }
+
+    deleteAppointment(id) {
+        this.appointments = this.appointments.filter( appointment => appointment.id !== id);
     }
 }
 
@@ -39,6 +42,64 @@ class UI {
         setTimeout(() => {
             divMessage.remove();
         }, 3000);
+    }
+
+    printAppointment({appointments}) {
+
+        cleanHTML();
+        
+        appointments.forEach(appointment => {
+            const { patientName, address, phone, date, hour, symptom, id } = appointment;
+            const divAppointment = document.createElement('div');
+
+            divAppointment.classList.add('styles');
+            divAppointment.dataset.id = id;
+
+            const patientNameParagraph = document.createElement('h2');
+            patientNameParagraph.classList.add('styles');
+            patientNameParagraph.textContent = patientName;
+
+            const addressParagraph = document.createElement('p');
+            addressParagraph.classList.add('styles');
+            addressParagraph.textContent = address;
+
+            const phoneParagraph = document.createElement('p');
+            phoneParagraph.classList.add('styles');
+            phoneParagraph.textContent = phone;
+
+            const dateParagraph = document.createElement('p');
+            dateParagraph.classList.add('styles');
+            dateParagraph.textContent = date;
+
+            const hourParagraph = document.createElement('p');
+            hourParagraph.classList.add('styles');
+            hourParagraph.textContent = hour;
+
+            const symptomParagraph = document.createElement('p');
+            symptomParagraph.classList.add('styles');
+            symptomParagraph.textContent = symptom;
+
+            const btnDelete = document.createElement('button');
+            btnDelete.classList.add('styles');
+            btnDelete.innerHTML = 
+                `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg> 
+                Delete`;
+
+            btnDelete.onclick = () => deleteAppointment(id);
+
+            divAppointment.appendChild(patientNameParagraph);
+            divAppointment.appendChild(addressParagraph);
+            divAppointment.appendChild(phoneParagraph);
+            divAppointment.appendChild(dateParagraph);
+            divAppointment.appendChild(hourParagraph);
+            divAppointment.appendChild(symptomParagraph);
+            divAppointment.appendChild(btnDelete);
+
+            medicalAppointments.appendChild(divAppointment);
+            
+        })
     }
 }
 
@@ -81,13 +142,14 @@ function newAppoinment(e) {
 
     objAppointment.id = Date.now();
 
-    adminAppointment.addApoinment({ ...objAppointment });
+    adminAppointment.addAppointment({ ...objAppointment });
 
     form.reset();
     
     restartObj();
 
     // Function to show html here
+    ui.printAppointment(adminAppointment);
     
 }
 
@@ -98,4 +160,18 @@ function restartObj() {
     objAppointment.date = '';
     objAppointment.hour = '';
     objAppointment.symptom = '';
+}
+
+function cleanHTML() {
+    while(medicalAppointments.firstChild) {
+        medicalAppointments.removeChild(medicalAppointments.firstChild);
+    }
+}
+
+function deleteAppointment(id) {
+    adminAppointment.deleteAppointment(id);
+
+    ui.printAlert('La cita se elimino correctamente');
+
+    ui.printAppointment(adminAppointment);
 }
